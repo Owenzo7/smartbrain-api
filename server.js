13f1428f13fee285191dd98250a5ce04b8cad737
Owenzo7 <owenlee545@gmail.com>
2023-06-05 21:13:18 +0300

    Done with api endpoints

---
+++ server.js
@@ -0,0 +1,100 @@
+// initialise the server
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@email.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "sally",
+      email: "sally@email.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  // Validation check from database
+
+  req.body.email === database.users[0].email &&
+  req.body.password === database.users[0].password
+    ? res.json(database.users[0])
+    : res.status(400).json("error logging in");
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    entries: 0,
+    joined: new Date(),
+  });
+
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+
+  if (!found) {
+    res.status(400).json("Not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+
+    if (!found) {
+      res.status(400).json("Not found");
+    }
+  });
+});
+
+app.listen(3004, () => {
+  console.log("app is running on port 3004");
+});
+
+/* 
+/signin ---> POST = success/fail
+/register ----> POST = user
+/profile/:userId --> GET = user
+/image ---> PUT ---> user
+
+*/
