cbc5bcf461021dc65f34fb92b114b7ac903c44d5
Owenzo7 <owenlee545@gmail.com>
2023-06-13 17:18:56 +0300

    files ready for bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+  entry: {
+    app: "./src/server.js",
+    image: "./src/controllers/image.js",
+    profile: "./src/controllers/profile.js",
+    register: "./src/controllers/register.js",
+    signin: "./src/controllers/signin.js",
   },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+};
+
+module.exports = {
+  mode: "production", // or 'production'
+  // rest of the webpack configuration
 };
