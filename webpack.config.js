013da62fa284ce98a1984e7b167aba66df2560ef
Owenzo7 <owenlee545@gmail.com>
2023-06-13 18:35:35 +0300

    made some few changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,11 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production", // or 'development'
-  entry: {
-    app: "./src/server.js",
-  },
+  mode: "production",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    filename: "bundle.js",
   },
-  resolve: {
-    fallback: {
-      timers: require.resolve("timers-browserify"),
-      tty: require.resolve("tty-browserify"),
-      url: require.resolve("url/"),
-      util: require.resolve("util/"),
-      path: require.resolve("path-browserify"),
-      stream: require.resolve("stream-browserify"),
-      buffer: require.resolve("buffer/"),
-      fs: false,
-      assert: require.resolve("assert/"),
-      querystring: require.resolve("querystring-es3"),
-      crypto: require.resolve("crypto-browserify"),
-      zlib: require.resolve("browserify-zlib"),
-      http: require.resolve("stream-http"),
-      net: require.resolve("net-browserify"),
-      async_hooks: require.resolve("async_hooks"),
-    },
-  },
+  // Add additional configuration options as needed
 };
